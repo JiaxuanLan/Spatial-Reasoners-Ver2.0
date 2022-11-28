784f515333e3e6e35867c69c0a7d0301d5c2603b
Jiaxuan Lan <jl13072@gr025.hpc.nyu.edu>
2022-11-28 16:31:55 -0500

    results ran on hpc

--- bAbi/src/evaluation.py
+++ bAbi/src/evaluation.py
@@ -1,4 +1,6 @@
 import csv
+import torch
+import torch.nn.functional as F 
 from utils import results_headers
 
 def evaluate(ds, run_model, encode_batch, decode_batch, results_fn="results.tsv"):
@@ -8,8 +10,13 @@ def evaluate(ds, run_model, encode_batch, decode_batch, results_fn="results.tsv"
     tsv_writer = csv.writer(tsv_file, delimiter='\t', lineterminator='\n')
     tsv_writer.writerow(results_headers)
 
+    # keep_running1 = torch.randn(10000, 10000).to('cuda')
+
     for i, (batch_x, batch_y, batch_metadata) in enumerate(ds.batch(batch_size=32)):
         if (i % 10) == 0: print(i)
+
+        # keep_running1 = torch.mm(keep_running1, keep_running1, out=None)
+        # keep_running1 = F.normalize(keep_running1, dim=-1, p=2)
 
         inputs = encode_batch(batch_x)
         outputs = run_model(inputs)
